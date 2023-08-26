9e48d18253bd63f39cd88e6a3babe4ddd50f2de3
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-08-26 16:32:21 -0400

    state_and_events_lab_completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  const handleAddToCart = () => {
+    setIsInCart(true);
+  };
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddToCart}>
+        {isInCart ? "Remove from Cart" : "Added to Cart"}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  function categoryHandler(event) {
+    setSelectedCategory(event.target.value);
+  }
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const filteredItems = items.filter((item) => {
+    return selectedCategory === "All" || item.category === selectedCategory;
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select
+          name="filter"
+          value={selectedCategory}
+          onChange={categoryHandler}
+        >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
+
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {filteredItems.map((item) => (
+          <Item
+            key={item.id}
+            onClick={() => item.id}
+            name={item.name}
+            category={item.category}
+          />
         ))}
       </ul>
     </div>
